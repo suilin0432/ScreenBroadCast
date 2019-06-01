d39859061237e72d4906cc8cd2bba87e27e5bfc1
隋霖 <401569798@qq.com>
2019-06-02 00:04:42 +0800

    add some util functions

---
+++ UDPUtils/TCPServer.py
@@ -0,0 +1,15 @@
+from PIL import ImageGrab
+import socket
+import threading
+import time
+import zlib
+
+# 截图函数
+def grabScreen():
+    return ImageGrab.grab()
+
+# 压缩函数
+def compress(s):
+    return
+
+
